8fea713b466a574b26a26c64f3e34f550abf5e03
fortruce <>
2015-03-15 18:50:36 -0400

    add correct created/updated timestamps

--- migrations/20150312-1137-add-lists-table.up.sql
+++ migrations/20150312-1137-add-lists-table.up.sql
@@ -4,5 +4,5 @@ CREATE TABLE lists (
        name varchar(50),
        id    integer PRIMARY KEY DEFAULT nextval('lid_serial'),
        updated_at timestamp DEFAULT now(),
-       create_at timestamp DEFAULT now()
+       created_at timestamp DEFAULT now()
 );
