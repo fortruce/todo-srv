9051d24c19d8a409692764f133eea4eda3222309
fortruce <>
2015-03-16 14:15:30 -0400

    cascade delete todos from lists

--- migrations/20150312-1223-add-todos-table.up.sql
+++ migrations/20150312-1223-add-todos-table.up.sql
@@ -3,7 +3,7 @@ CREATE SEQUENCE tid_serial;
 CREATE TABLE todos (
        name varchar(50),
        id integer PRIMARY KEY DEFAULT nextval('tid_serial'),
-       lid integer REFERENCES lists(id),
+       lid integer REFERENCES lists(id) ON DELETE CASCADE,
        updated_at timestamp DEFAULT now(),
        created_at timestamp DEFAULT now()
 );
